f880afa48125fef8b4c49baaf9d4260457e961c3
stogashi146 <stogashi146@gmail.com>
2021-07-16 09:07:08 +0000

    [Update] 管理者：顧客一覧、詳細

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,13 @@
 class Admin::CustomersController < ApplicationController
+
+   before_action :authenticate_admin!
+
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,10 +3,12 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many	:addresses, dependent: :destroy	
-  has_many	:orders, dependent: :destroy	
+
+  has_many	:addresses, dependent: :destroy
+  has_many	:orders, dependent: :destroy
   has_many	:cart_products, foreign_key: "customer_id",	dependent: :destroy
   has_many	:products, through: :cart_products,	source: :product
-  
+
+  paginates_per 10
+
 end
