4489c4ff3d2a3db2d6c14377f82b5877490f4399
maruyu31 <you3746336@gmail.com>
2021-07-17 04:24:22 +0000

    [Update]管理者側の商品ページ

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,47 @@
 class Admin::ProductsController < ApplicationController
-  def index
-  end
 
-  def new
+  before_action :authenticate_admin!
+
+  def index
+    @products = Product.all
+    @products_page = Product.page(params[:page]).per(10)
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+    @genres = Genre.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: "新しい商品を追加しました！"
+    else
+      render "new"
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
+    @genres = Genre.all
   end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: "商品を編集しました！"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
+    end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '919881b1f25b287089b8b2c5b7cfdfcda20aab6e793a7da3617c7aab5c90c716a6c0ab0e3e30bc607c8d65287331cfb8a7830962d08e412bbfde358bc0c88b1d'
