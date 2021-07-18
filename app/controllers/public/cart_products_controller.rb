4c919b0cb05744cc0b3f2942d48a854330bca920
EC2 Default User <ec2-user@ip-172-31-62-14.ec2.internal>
2021-07-18 04:00:08 +0000

    [Update] カート一覧機能

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,4 +1,38 @@
 class Public::CartProductsController < ApplicationController
+
+  before_action :authenticate_customer!
+
+
   def index
+    @products = current_customer.cart_products
+    # @cart_products = current_customer.cart_products
   end
+
+  def create
+
+
+    @cart_product.amount += params[:amount].to_i
+    @cart_product.save
+    redirect_to cart_products_path
+  end
+
+  def update
+    @cart_product = CartProduct.find(params[:id])
+    @cart_product.update(amount: params[:cart_product][:amount].to_i)
+    redirect_to cart_products_path
+  end
+
+  def destroy
+    @cart_product.destroy
+    redirect_to current_cart
+  end
+
+  def destroy_all
+    @cart_products = current_cart.cart_products
+    @cart_products.destroy_all
+    redirect_to cart_products_path, notice: "商品を全て削除しました。"
+  end
+
+
+
 end
