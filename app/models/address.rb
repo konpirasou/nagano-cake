4b3962ed295e899dbb7dbddbafb40cdc8f50591d
stogashi146 <stogashi146@gmail.com>
2021-07-18 05:15:59 +0000

    [Update] 会員側：配送先一覧、削除実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,9 @@ class Address < ApplicationRecord
 
   belongs_to	:customer
 
+  validates :customer_id, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
