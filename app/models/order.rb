dadd4065945084931212212fc21f31a0391b2893
stogashi146 <stogashi146@gmail.com>
2021-07-19 10:05:28 +0000

    [Update] 会員　注文履歴一覧、詳細

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   has_many	 :order_products,	foreign_key: "order_id", dependent: :destroy
   has_many	 :products,	through: :order_products, source: :product
 
-  enum statuses: { 0 => "着手不可", 1 => "制作待ち", 2 => "製作中", 3 => "制作完了" }, _prefix: true
+  enum statuses: { 0 => "入金待ち", 1 => "入金確認", 2 => "製作中", 3 => "発送準備中", 4 => "発送済み" }, _prefix: true
   enum payment_methods: { 0 => "クレジットカード", 1 => "銀行振込" }, _prefix: true
 
 end
