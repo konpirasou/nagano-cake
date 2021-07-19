b537dc94e6104471f917c4562977917f121f0786
stogashi146 <stogashi146@gmail.com>
2021-07-19 09:12:50 +0000

    [Update] 会員：注文履歴一覧、詳細

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
 
   belongs_to :customer
-  has_many	 :order_products,	foreign_key: "product_id", dependent: :destroy
-  has_many	 :products,	through: order_products, source: :product
+  has_many	 :order_products,	foreign_key: "order_id", dependent: :destroy
+  has_many	 :products,	through: :order_products, source: :product
+
+  enum statuses: { 0 => "着手不可", 1 => "制作待ち", 2 => "製作中", 3 => "制作完了" }, _prefix: true
+  enum payment_methods: { 0 => "クレジットカード", 1 => "銀行振込" }, _prefix: true
 
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,9 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
+
 end
