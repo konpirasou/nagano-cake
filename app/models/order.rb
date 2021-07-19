bb6981103f53064c98b7a6677abbd81fdf78b011
stogashi146 <stogashi146@gmail.com>
2021-07-19 11:11:51 +0000

    [Update] enum記述方法修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   has_many	 :order_products,	foreign_key: "order_id", dependent: :destroy
   has_many	 :products,	through: :order_products, source: :product
 
-  enum statuses: { 0 => "入金待ち", 1 => "入金確認", 2 => "製作中", 3 => "発送準備中", 4 => "発送済み" }, _prefix: true
-  enum payment_methods: { 0 => "クレジットカード", 1 => "銀行振込" }, _prefix: true
+  enum  payment_method: { "クレジットカード": 0, "銀行振込": 1}
+  enum  status: {"入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4}
 
 end
