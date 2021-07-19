618cc450214f0e409607830dc086396195fe2a52
stogashi146 <stogashi146@gmail.com>
2021-07-19 09:58:25 +0000

    [Update] 会員　注文履歴一覧、詳細

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,8 +9,14 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.where(customer_id: current_customer.id)
   end
 
   def show
+    @order = Order.find(params[:id])
+    @total_price = 0
+    @order.order_products.each do |product|
+      @total_price += (product.add_tax_price * product.amount)
+    end
   end
 end
