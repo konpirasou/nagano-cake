0b693dcecd74c917c229da1d313af1ed927cb847
maruyu31 <you3746336@gmail.com>
2021-07-19 12:16:00 +0000

    [Update]管理者側の注文履歴

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,10 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
+  enum  product_status: {"着手不可": 0, "制作待ち": 1, "制作中": 2, "制作完了": 3}
+
 end
